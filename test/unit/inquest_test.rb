0981eb1bc55035fa0cdd4af20ef6452e92d4d192
James A. Rosen <james.a.rosen@gmail.com>
2010-01-17 15:35:57 -0500

    refactored InquestTest to use more specific assertions about validity

--- test/unit/inquest_test.rb
+++ test/unit/inquest_test.rb
@@ -3,15 +3,18 @@ require 'test_helper'
 class InquestTest < ActiveSupport::TestCase
   
   test 'a new Inquest should require an image_url' do
-    assert !Inquest.new(:image_url => nil).valid?
+    i = Inquest.create(:image_url => nil)
+    assert i.errors[:image_url].present?
   end
   
   test "a new Inquest with a non-fully-qualified image_url should not be valid" do
-    assert !Inquest.new(:image_url => '/my_image').valid?
+    i = Inquest.create(:image_url => '/foo')
+    assert i.errors[:image_url].present?
   end
   
   test "a new Inquest with an image_url that's an absolute URL should be valid" do
-    assert Inquest.new(:image_url => 'http://imageserver.com/12345.png').valid?
+    i = Inquest.create(:image_url => 'http://myimageserver.com/images/1.png')
+    assert i.errors[:image_url].blank?
   end
   
 end
