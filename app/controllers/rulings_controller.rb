3f7ebb315dfafdd73b1b56e3ceacfcc3def582e6
James A. Rosen <james.a.rosen@gmail.com>
2010-01-17 23:57:34 -0500

    actually marked rulings off in current session so user doesn't double-vote

--- app/controllers/rulings_controller.rb
+++ app/controllers/rulings_controller.rb
@@ -4,6 +4,7 @@ class RulingsController < ApplicationController
     @ruling ||= Ruling.new(params[:ruling])
     if @ruling.save
       redirect_to random_inquest_path
+      current_user_has_ruled_on_inquest(@ruling.inquest)
     else
       @inquest = @ruling.inquest
       render :action => '/inquests/show'
--- test/functional/rulings_controller_test.rb
+++ test/functional/rulings_controller_test.rb
@@ -1,5 +1,11 @@
 require 'test_helper'
 
+ApplicationController.class_eval do
+  def current_user_has_ruled_on_inquest?(i)
+    inquest_ids_ruled_on_by_current_user.include?(i.id)
+  end
+end
+
 class RulingsControllerTest < ActionController::TestCase
   
   setup do
@@ -11,6 +17,11 @@ class RulingsControllerTest < ActionController::TestCase
   test "after successfully ruling on an Inquest, the user should be redirected to another random Inquest" do
     post :create, :ruling => { :inquest_id => @inquest1.id, :vote => 'yes' }
     assert_redirected_to random_inquest_path
+  end
+  
+  test "after successfully ruling on an Inquest, should mark the current user as having ruled on it" do
+    post :create, :ruling => { :inquest_id => @inquest2.id, :vote => 'no' }
+    assert @controller.current_user_has_ruled_on_inquest?(@inquest2)
   end
   
 end
