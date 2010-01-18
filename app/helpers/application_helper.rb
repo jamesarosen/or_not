6d24a793332230b4e0224c3ff595e7ea36647393
James A. Rosen <james.a.rosen@gmail.com>
2010-01-17 20:49:49 -0500

    changed form on inquests#show to use #button_to; added 'maybe' button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,18 @@ module ApplicationHelper
     url_for({:controller => :inquests, :action => :create}.merge(options))
   end
   
+  # TODO: remove once
+  # https://rails.lighthouseapp.com/projects/8994/tickets/3730
+  # is resolved.
+  def rulings_path(options = {})
+    rulings_url(options.merge({:only_path => true}))
+  end
+  
+  # TODO: remove once
+  # https://rails.lighthouseapp.com/projects/8994/tickets/3730
+  # is resolved.
+  def rulings_url(options)
+    url_for({:controller => :rulings, :action => :create}.merge(options))
+  end
+  
 end
