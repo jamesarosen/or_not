876306aec5ff0672498a9f64a2f4be8d33e33a2f
James A. Rosen <james.a.rosen@gmail.com>
2010-01-18 00:14:31 -0500

    added some flash feedback on voting

--- app/controllers/rulings_controller.rb
+++ app/controllers/rulings_controller.rb
@@ -3,9 +3,11 @@ class RulingsController < ApplicationController
   def create
     @ruling ||= Ruling.new(params[:ruling])
     if @ruling.save
+      flash[:notice] = I18n.t('rulings.create.success')
       redirect_to random_inquest_path
       current_user_has_ruled_on_inquest(@ruling.inquest)
     else
+      flash[:error] = I18n.t('rulings.create.error')
       @inquest = @ruling.inquest
       render :action => '/inquests/show'
     end
